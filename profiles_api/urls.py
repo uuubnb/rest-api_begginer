944b865221bb7ec2499afe534a0948e35bbb6316
uuubnb <antontit89@gmail.com>
2020-11-22 19:18:55 +0700

    Added Login API & Token retrieving

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -9,8 +9,8 @@ router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profiles-viewset', views.UserProfileViewSet)
 
-
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
